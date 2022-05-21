f7051ff3c3f113b100b97e01a066033e726f099e
julia <julia.kirichuk@gmail.com>
2022-05-21 03:37:52 +0300

    Deployment final settings

--- deploy/update.sh
+++ deploy/update.sh
@@ -1,8 +1,9 @@
+
 #!/usr/bin/env bash
 
 set -e
 
-PROJECT_BASE_PATH='/usr/local/apps/rest-api-mark'
+PROJECT_BASE_PATH='/usr/local/apps/profiles-rest-api'
 
 git pull
 $PROJECT_BASE_PATH/env/bin/python manage.py migrate
